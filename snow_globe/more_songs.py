dc0561c4e1aefadcd1fbae0c1d4b3614c0bc5615
Marissa <Marissa@UtterbergDataDev.com>
2018-10-11 15:47:20 -0400

    Create more_songs.py

---
+++ snow_globe/more_songs.py
@@ -0,0 +1,31 @@
+# More songs for your holiday Snow Globe!
+
+joy_to_the_world = []
+
+auld_lang_syne = []
+
+deck_the_halls = []
+
+we_wish_you_a_merry_christmas = []
+
+come_all_ye_faithful = []
+
+o_tanenbaum = []
+
+here_comes_santa_claus = []
+
+little_drummer_boy = []
+
+we_three_kings = []
+
+rudolp_the_red_nosed_reindeer = []
+
+here_we_come_a_wassailing = []
+
+silver_bells = []
+
+nat_king_cole_christmas_song = []
+
+dreidel = []
+
+hava_nagila = []

d61651e59c3c979a22af1c8b31660fe4733af945
Marissa <Marissa@UtterbergDataDev.com>
2018-10-11 15:53:25 -0400

    Update more_songs.py

--- snow_globe/more_songs.py
+++ snow_globe/more_songs.py
@@ -1,31 +1,168 @@
-# More songs for your holiday Snow Globe!
+# Songs for the section marked...
 
-joy_to_the_world = []
+## =====================================
+## We are defining ALL of our music here
+## =====================================
 
+# Set up time signature
+
+whole_note = 1.5  # adjust this to change tempo of everything
+# other notes are fractions of the whole note
+half_note = whole_note / 2
+quarter_note = whole_note / 4
+dotted_quarter_note = quarter_note * 1.5
+eighth_note = whole_note / 8
+
+# Set up note values
+
+C3 = 131
+G3 = 196
+A3 = 220
+Bb3 = 233
+B3 = 247
+C4 = 262
+Db4 = 277
+D4 = 294
+Eb4 = 311
+E4 = 330
+F4 = 349
+Gb4 = 370
+G4 = 392
+Ab4 = 415
+A4 = 440
+Bb4 = 466
+B4 = 494
+C5 = 523
+D5 = 587
+E5 = 659
+
+# Set up the songs!
+
+# Jingle Bells
+jingle_bells_song = [
+        [E4, quarter_note],
+        [E4, quarter_note],
+        [E4, half_note],
+        [E4, quarter_note],
+        [E4, quarter_note],
+        [E4, half_note],
+        [E4, quarter_note],
+        [G4, quarter_note],
+        [C4, dotted_quarter_note],
+        [D4, eighth_note],
+        [E4, whole_note],
+    ]
+
+# Let It Snow
+let_it_snow_song = [
+    [B4, eighth_note],
+    [A4, eighth_note],
+    [G4, quarter_note],
+    [G4, eighth_note],
+    [F4, eighth_note],
+    [E4, quarter_note],
+    [E4, eighth_note],
+    [D4, eighth_note],
+    [C4, whole_note],
+    [G3, eighth_note],
+    [G3, eighth_note],
+    [G4, eighth_note],
+    [G4, eighth_note],
+    [F4, quarter_note],
+    [E4, quarter_note],
+    [D4, eighth_note],
+    [C4, quarter_note],
+    [G3, whole_note],
+
+]
+
+# Linus & Lucy (A Peanuts Christmas song)
+linus_and_lucy_song = [
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [C4, eighth_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [C4, dotted_quarter_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [A3, eighth_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [A3, dotted_quarter_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [C4, eighth_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [C4, dotted_quarter_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [A3, eighth_note],
+    [C3, eighth_note],
+    [G3, eighth_note],
+    [A3, dotted_quarter_note],
+    [C5, quarter_note],
+    [D5, eighth_note],
+    [E5, quarter_note],
+    [E5, eighth_note],
+    [D5, eighth_note],
+    [C5, quarter_note],
+    [D5, dotted_quarter_note],
+    [C5, half_note],
+    [C5, quarter_note],
+    [D5, eighth_note],
+    [E5, whole_note],
+]
+
+# The songs above were pre-loaded to your code.py
+# Here are some more songs for your holiday Snow Globe!
+
+# Joy to the World
+joy_to_the_world = [
+    [C5, whole_note],
+    [C4, whole_note],
+]
+
+# Auld Lang Syne
 auld_lang_syne = []
 
+
+# Deck the Halls
 deck_the_halls = []
 
+# We Wish You a Merry Christmas
 we_wish_you_a_merry_christmas = []
 
+# Come All Ye Faithful
 come_all_ye_faithful = []
 
+# O Tanenbaum
 o_tanenbaum = []
 
+# Here Comes Santa Claus
 here_comes_santa_claus = []
 
+# Little Drummer Boy
 little_drummer_boy = []
 
+# We Three Kings
 we_three_kings = []
 
+# Rudolph the Red-Nosed Reindeer
 rudolp_the_red_nosed_reindeer = []
 
+# Here We Come A-Wassailing
 here_we_come_a_wassailing = []
 
+# Silver Bells
 silver_bells = []
 
+# The Christmas Song
 nat_king_cole_christmas_song = []
 
+# Dreidel, Dreidel, Dreidel
 dreidel = []
 
+# Hava Nagila
 hava_nagila = []
